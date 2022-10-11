575ae26fe5cc8bc96740f140d1f40a71f3c03305
brunomaschietto <bruno4_10@hotmail.com>
2022-10-11 19:03:17 -0300

    Exercicios aula 31 resolvidos

---
+++ exercicio-componentes/src/components/InfosUsuario.js
@@ -0,0 +1,8 @@
+function InfosUsuario() {
+    return <div>
+        <img src="https://i0.wp.com/www.portaldodog.com.br/cachorros/wp-content/uploads/2021/03/visa%CC%83o-do-cachorro-2.jpeg?resize=626%2C626&ssl=1" id="foto"></img>
+        <h3>Criado por Bruno Maschietto</h3>
+    </div>
+}
+
+export default InfosUsuario;
